44793a3f338124a18e6e00f81414d934f30bbe7f
lucaslprimo <lucaslprimo@gmail.com>
2021-10-10 12:12:05 -0300

    Pac Man Mecs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Player : MonoBehaviour
+
+
+namespace gamejamplus2020_t9
 {
-    [SerializeField] public Camera mainCamera;
-    [SerializeField] public bool isEnable = true;
-    
-    void Start()
+    public class Player : MonoBehaviour
     {
-        
-    }
+        [SerializeField] public Camera mainCamera;
+        [SerializeField] public bool isEnable = true;
+        [SerializeField] public float powerUpDuration;
 
-    void Update()
-    {
-        
+        private float timeToStopPowerUp;
+
+
+        private Painter painter;
+
+        public PlayerState playerState;
+
+        public UnityEvent OnPowerUpCatched;
+        public UnityEvent OnPlayerDies;
+
+        public enum PlayerState
+        {
+            Chaser, Runner
+        }
+
+        void Start()
+        {
+            painter = gameObject.GetComponent<Painter>();
+            playerState = PlayerState.Runner;
+        }
+
+        void Update()
+        {
+            if(playerState == PlayerState.Chaser)
+            {
+                if (Time.time >= timeToStopPowerUp)
+                {
+                    playerState = PlayerState.Runner;
+                }
+            }
+           
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("PowerUp"))
+            {
+                timeToStopPowerUp = Time.time + powerUpDuration;
+                playerState = PlayerState.Chaser;
+                Destroy(other.gameObject);
+                if (OnPowerUpCatched != null)
+                    OnPowerUpCatched.Invoke();
+            }
+
+            if (other.CompareTag("Enemy"))
+            {
+                if (playerState == PlayerState.Runner)
+                {
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    if (painter.CheckSurfacePainted())
+                    {
+                        Destroy(other.gameObject);
+                    }
+                }
+            }
+        }
     }
 }

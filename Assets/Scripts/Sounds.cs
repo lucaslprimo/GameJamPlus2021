d2345064f0c531ddd3e3ae71f368bdb712e8bc87
Demetrio <resetgamesstudio@hotmail.com>
2021-10-10 21:23:07 -0300

    Ajustes da iluminação, Power Up

--- Assets/Scripts/Sounds.cs
+++ Assets/Scripts/Sounds.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Sounds : MonoBehaviour
 {
     public void deadPlayer()
     {
         //Ativa o Som do FMOD
         //FMODUnity.RuntimeManager.PlayOneShot("event:/PlayerSteps", GetComponent<Transform>().position);
-
     }
 }
